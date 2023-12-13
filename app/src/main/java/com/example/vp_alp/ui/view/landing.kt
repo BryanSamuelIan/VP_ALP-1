0ab97408295c10c53e22a9646d3dc044bf208c2d
BryanSamuelIan <bryansamuelangka@gmail.com>
2023-12-13 18:02:55 +0700

    fix dikit

--- app/src/main/java/com/example/vp_alp/ui/view/landing.kt
+++ app/src/main/java/com/example/vp_alp/ui/view/landing.kt
@@ -1,23 +1,17 @@
 package com.example.vp_alp.ui.view
 
-import android.widget.Toast
+
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.ui.res.painterResource
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,9 +24,6 @@ fun landing() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .clickable {
-                Toast.makeText(context, "Do something", Toast.LENGTH_SHORT).show()
-            }
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
